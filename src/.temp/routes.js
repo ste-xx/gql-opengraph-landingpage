487b5986be88062d16bad7043e441508d3cc4a71
ste-xx <stefan.breitenstein@gmail.com>
2020-04-14 21:38:13 +0200

    gridsome

--- src/.temp/routes.js
+++ src/.temp/routes.js
@@ -1,5 +1,13 @@
 export default [
   {
+    path: "/projects/",
+    component: () => import(/* webpackChunkName: "page--src-pages-projects-vue" */ "C:\\Users\\stefa\\IdeaProjects\\stefan.breitenstein\\src\\pages\\Projects.vue")
+  },
+  {
+    path: "/blog/",
+    component: () => import(/* webpackChunkName: "page--src-pages-blog-vue" */ "C:\\Users\\stefa\\IdeaProjects\\stefan.breitenstein\\src\\pages\\Blog.vue")
+  },
+  {
     path: "/about/",
     component: () => import(/* webpackChunkName: "page--src-pages-about-vue" */ "C:\\Users\\stefa\\IdeaProjects\\stefan.breitenstein\\src\\pages\\About.vue")
   },
