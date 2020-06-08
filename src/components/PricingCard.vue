a60a03e8f53e0c3107746a5a445ff76f78455612
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2020-06-08 17:50:37 +0200

    landingpage for opengql

---
+++ src/components/PricingCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="pricing-card">
+    <div class="pricing-card--plan-name">
+      {{ planName }}
+    </div>
+    <div class="pricing-card--price">
+      {{ price }}
+    </div>
+    <div class="pricing-card--description">
+      <slot name="description" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    planName: {
+      type: String,
+      default: 0
+    },
+    price: {
+      type: String,
+      default: 0
+    }
+  }
+};
+</script>
+
+<style>
+.pricing-card {
+  display: grid;
+  box-shadow: var(--card-shadow);
+  grid-template-areas:
+    '.'
+    'planName'
+    'price'
+    'description';
+  grid-template-rows: min(2rem, 16px) auto auto;
+  grid-template-columns: auto;
+  width: 800px;
+  background-color: white;
+}
+
+.pricing-card--plan-name {
+  grid-area: planName;
+  color: var(--card-title-color);
+  font-size: var(--card-title-font-size);
+  font-family: var(--card-title-font-family);
+  font-weight: var(--card-title-font-weight);
+  line-height: var(--card-title-line-height);
+  margin: var(--card-content-margin);
+}
+
+.pricing-card--price {
+  grid-area: price;
+}
+
+.pricing-card--description {
+  grid-area: description;
+  margin: var(--card-content-margin);
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,161 +1,172 @@
 <template>
-    <Layout class="background-container">
-        <div style="margin-top: 30px;">
-            <g-image style="object-fit: cover; border-radius:50%; float: left; margin-top: 1.5em; margin-right: 2em; "
-                     src="~/assets/avatar.svg" width="120"/>
-        </div>
-        <h4 id="introduction" class="introduction">Open GQL</h4>
-        <p style="margin-top: 0; margin-bottom: min(2.5rem, 16px);" class="font-xl">Open Graph meets GraphQL</p>
-        <p id="tagline" class="font-xl">
-            All you want to know about a link in a single Request and without a backend.
-        </p>
-        <div class="useCases font-l">
-            <div>Create preview links like in whatsapp or facebook from your app.</div>
-            <div>Get relevant infos about a link.</div>
-        </div>
-        <div id="gettingStarted" class="gettingStarted">
-            <h6 class="font-l-alt">
-                Getting Started
-            </h6>
-            <preview-card style="width: 100%">
-                <template #description>
-                    <showcase/>
-                </template>
-            </preview-card>
-        </div>
-        <h6 class="font-l-alt">
-            Why GQL?
-        </h6>
-        <preview-card id="compTable" style="width: 100%; margin: 0;">
-            <template #description>
-                <compare-table/>
-            </template>
-        </preview-card>
-        <h6 class="font-l-alt">
-           Pricing
-        </h6>
-        <preview-card id="freePricing" style="width: 100%;">
-            <template #title>
-                <div class="center">
-                    Free Plan
-                </div>
-            </template>
-            <template #description>
-                - basic types (title, x)
-                - rate limited with
-                - use without an api key
-            </template>
-        </preview-card>
-        <div>&nbsp;</div>
+  <Layout class="background-container">
+    <div style="margin-top: 30px;">
+      <g-image style="object-fit: cover; border-radius: 50%; float: left; margin-top: 1.5em; margin-right: 2em;" src="~/assets/avatar.svg" width="120" />
+    </div>
+    <h4 id="introduction" class="introduction">Open GQL</h4>
+    <p style="margin-top: 0; margin-bottom: min(2.5rem, 16px);" class="font-xl">Open Graph meets GraphQL</p>
+    <p id="tagline" class="font-xl">
+      All you want to know about a link in a single Request and without a backend.
+    </p>
+    <div class="useCases font-l">
+      <div>Create preview links like in whatsapp or facebook from your app.</div>
+      <div>Get relevant infos about a link.</div>
+    </div>
+    <div id="gettingStarted" class="gettingStarted">
+      <h6 class="font-l-alt">
+        Getting Started
+      </h6>
+      <preview-card style="width: 100%;">
+        <template #description>
+          <showcase />
+        </template>
+      </preview-card>
+    </div>
+    <h6 class="font-l-alt">
+      Why GQL?
+    </h6>
+    <preview-card id="compTable" style="width: 100%; margin: 0;">
+      <template #description>
+        <compare-table />
+      </template>
+    </preview-card>
+    <h6 class="font-l-alt">
+      Pricing
+    </h6>
 
-        <preview-card id="premiumPlan" style="width: 100%;">
-            <template #title>
-                <div class="center">
-                    Premium Plan
-                </div>
-            </template>
-            <template #description>
-                - additional types like video
-                - dedicated 10.000 request per day assured
-                - price: 4.99
-            </template>
-        </preview-card>
+    <div style="display: flex; width: 100%; justify-content: space-between; flex-wrap: wrap">
+      <pricing-card id="freePricing" class="pricing-card" plan-name="Free Plan" price="free">
+        <template #description>
+          <div>following basic types:</div>
+          <ul>
+            <li>og:title</li>
+            <li>og:type</li>
+            <li>og:image</li>
+            <li>og:url</li>
+          </ul>
+          <div>usable without an api key</div>
+          <div>global rate limited</div>
+        </template>
+      </pricing-card>
 
-        <div>&nbsp;</div>
-        <preview-card id="enterprisePlan" style="width: 100%;">
-            <template #title>
-                <div class="center">
-                    Enterprise Plan
-                </div>
-            </template>
-            <template #description>
-                - special requirements or consulting fundamental more required requests.
-                - contact
-            </template>
-        </preview-card>
-    </Layout>
+      <pricing-card id="premiumPlan" class="pricing-card" plan-name=" Premium Plan" price="4,99$ per month">
+        <template #description>
+          <div>all in free plan + following additional types:</div>
+          <ul>
+            <li>og:audio</li>
+            <li>og:description</li>
+            <li>og:determiner</li>
+            <li>og:locale</li>
+            <li>og:site_name</li>
+            <li>og:video</li>
+          </ul>
+          <div>dedicated 5.000 request per day assured</div>
+          <a class="waves-effect waves-light btn-large light-blue lighten-4 grey-text text-darken-2">Choose Premium</a>
+        </template>
+      </pricing-card>
+
+      <pricing-card id="enterprisePlan" class="pricing-card" plan-name="Enterprise Plan" price="Please contact">
+        <template #description>
+          - special requirements or consulting fundamental more required requests. - contact
+          <a class="waves-effect waves-light btn-large light-blue lighten-4 grey-text text-darken-2">Choose Enterprise</a>
+        </template>
+      </pricing-card>
+    </div>
+  </Layout>
 </template>
 
 <script>
-  import PreviewCard from '~/components/PreviewCard.vue'
-  import Showcase from '~/components/Showcase.vue'
-  import CompareTable from '~/components/CompareTable.vue'
-
-  export default {
-    components: {
-      PreviewCard,
-      Showcase,
-      CompareTable
-    },
-    metaInfo: {
-      title: 'Open Graph meets GraphQL'
-    },
-    methods: {}
-  }
+import PreviewCard from '~/components/PreviewCard.vue';
+import PricingCard from '~/components/PricingCard.vue';
+import Showcase from '~/components/Showcase.vue';
+import CompareTable from '~/components/CompareTable.vue';
+// 40 * 13 / 3
+export default {
+  components: {
+    PreviewCard,
+    PricingCard,
+    Showcase,
+    CompareTable
+  },
+  metaInfo: {
+    title: 'Open Graph meets GraphQL'
+  },
+  methods: {}
+};
 </script>
 
 <style>
-    .home-links a {
-        margin-right: 1rem;
-    }
+.home-links a {
+  margin-right: 1rem;
+}
 
-    .introduction {
-        font-family: 'Raleway', sans-serif;
-        font-weight: 600;
-        font-style: italic;
-        font-size: 32pt;
-        color: var(--header-color);
-        line-height: 1.2em;
-        margin-top: 50px;
-        margin-bottom: 0;
-    }
+.introduction {
+  font-family: 'Raleway', sans-serif;
+  font-weight: 600;
+  font-style: italic;
+  font-size: 32pt;
+  color: var(--header-color);
+  line-height: 1.2em;
+  margin-top: 50px;
+  margin-bottom: 0;
+}
 
-    .gettingStarted {
-        margin-top: 32px;
-        margin-bottom: 2rem;
-    }
+.gettingStarted {
+  margin-top: 32px;
+  margin-bottom: 2rem;
+}
 
-    .font-l {
-        font-size: 1.3em;
-    }
+.font-l {
+  font-size: 1.3em;
+}
 
-    .font-l-alt {
-        font-size: 32pt;
-    }
+.font-l-alt {
+  font-size: 32pt;
+}
 
-    .font-xl {
-        font-size: 1.5em;
-    }
+.font-xl {
+  font-size: 1.5em;
+}
 
-    .font-xl-alt {
-        font-size: 36pt;
-    }
+.font-xl-alt {
+  font-size: 36pt;
+}
 
-    .useCases div:nth-child(2n) {
-        margin-top: 16px;
-        float: right
-    }
+.useCases div:nth-child(2n) {
+  margin-top: 16px;
+  float: right;
+}
 
-    @media only screen and (min-width: 768px) {
-        .useCases div:nth-child(2n) {
-            float: left;
-        }
-    }
+@media only screen and (min-width: 768px) {
+  .useCases div:nth-child(2n) {
+    float: left;
+  }
+}
 
-    .useCases::after {
-        content: " ";
-        display: block;
-        height: 0;
-        clear: both;
-    }
+.useCases::after {
+  content: ' ';
+  display: block;
+  height: 0;
+  clear: both;
+}
 
-    #compTable .preview-card--description {
-        margin: 0;
-    }
+#compTable .preview-card--description {
+  margin: 0;
+}
 
-    .center {
-        display: flex;
-        justify-content: center;
-    }
+.center {
+  display: flex;
+  justify-content: center;
+}
 
+.pricing-card {
+  width: 100%;
+  margin-top: 16px;
+}
+
+@media only screen and (min-width: 768px) {
+  .pricing-card {
+    width: 30%;
+  }
+}
 </style>
